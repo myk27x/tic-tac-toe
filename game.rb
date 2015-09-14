975921538c24390d8c30b3c91b4367f810663a60
Michael Reed <myk27x@yahoo.com>
2015-09-14 02:01:19 -0400

    added advanced ai, reworked user prompts, needs classes and code optimization

--- game.rb
+++ game.rb
@@ -57,34 +57,57 @@ class Game
 
   def player_init_1p
     puts "What's your name?"
-    name = gets.chomp.capitalize
+    @name = gets.chomp.capitalize
     puts "Do you want 'X' or 'O'?"
     x_or_o = gets.chomp.upcase
     if x_or_o == "X"
-      @p1 = Player1.new(name)
-      play_1p_X
+      @name1 = @name
+      puts "Ok, #{@name}, choose your game mode:"
+      puts "(E)asy or (A)dvanced?"
+      which_ai = gets.chomp.capitalize
+      if which_ai == "E"
+        @p1 = Player1.new(@name1)
+        play_1p_X
+      elsif which_ai == "A"
+        @p1adv = Player1.new(@name1)
+        play_1p_Adv_X
+      else
+        puts "Well... you messed up... now we have to start over..."
+        player_init_1p
+      end
     elsif x_or_o == "O"
-      @p2 = Player2.new(name)
-      play_1p_O
+      @name2 = @name
+      puts "Ok, #{@name}, choose your game mode:"
+      puts "(E)asy or (A)dvanced?"
+      which_ai = gets.chomp.capitalize
+      if which_ai == "E"
+        @p2 = Player2.new(@name2)
+        play_1p_O
+      elsif which_ai == "A"
+        @p2adv = Player2.new(@name2)
+        play_1p_Adv_O
+      else
+        puts "Well... you messed up... now we have to start over..."
+        player_init_1p
+      end
     else
-      puts "No, no, no, you have to pick 'X' or 'O'..."
+      puts "Well... you messed up... now we have to start over..."
       player_init_1p
     end
   end
 
   def player_init_2p
     puts "Who is going to be 'X'?"
-    name = gets.chomp.capitalize
-    @p1 = Player1.new(name)
+    @name1 = gets.chomp.capitalize
+    @p1 = Player1.new(@name1)
     puts "Who is going to be 'O'?"
-    name = gets.chomp.capitalize
-    @p2 = Player2.new(name)
+    @name2 = gets.chomp.capitalize
+    @p2 = Player2.new(@name2)
     puts ""
-    puts "#{@p1.name} is 'X'"
+    puts "#{@name1} is 'X'"
     puts ""
-    puts "#{@p2.name} is 'O'"
+    puts "#{@name2} is 'O'"
     puts ""
-    # board
     play_2p
   end
 
@@ -107,16 +130,103 @@ class Game
     play_again
   end
 
-  def ai_O
-    @ai_choice = rand(1..9)
+  def puts_X_down
+    puts "#{@name1}'s turn."
+    print "Enter your choice:"
+    @p1_choice = gets.chomp.to_i
     @cells.map do |cell|
-      if cell == @ai_choice
-        @cells[@ai_choice - 1] = :O
-        puts "Computer chooses #{@ai_choice}."
+      if cell == @p1_choice
+        @cells[@p1_choice - 1] = :X
         return true
       end
     end
     return false
+  end
+
+  def play_1p_Adv_X
+    board
+    loop do
+      until puts_X_down
+        puts "Try again!"
+      end
+      @count += 1
+      board
+      break if @count ==9
+      until ai_adv_O
+      end
+      @count += 1
+      board
+      break if @count ==9
+    end
+    puts "It's a tie!"
+    play_again
+  end
+
+  def ai_adv_O
+    if check_win_O == true
+      return true
+    elsif check_loss_O == true
+      return true
+    else ai_O
+      return true
+    end
+  end
+
+  def check_win_O
+    @win2.each do |inner|
+      smart_ai = inner.reject do |value|
+        value == :C
+      end
+      smart_ai.select! do |value|
+        if smart_ai.length == 1 && value != :X
+          @ai_choice = value
+          @cells.map do |cell|
+            if cell == @ai_choice
+              @cells[@ai_choice - 1] = :O
+              puts "Computer chooses #{@ai_choice}."
+              return true
+            end
+          end
+        else
+          value
+        end
+      end
+    end
+  end
+
+  def check_loss_O
+    @win2.each do |inner|
+      smart_ai = inner.reject do |value|
+        value == :X
+      end
+      smart_ai.select! do |value|
+        if smart_ai.length == 1 && value != :C
+          @ai_choice = value
+          @cells.map do |cell|
+            if cell == @ai_choice
+              @cells[@ai_choice - 1] = :O
+              puts "Computer chooses #{@ai_choice}."
+              return true
+            end
+          end
+        else
+          value
+        end
+      end
+    end
+  end
+
+  def ai_O
+    loop do
+      @ai_choice = rand(1..9)
+      @cells.map do |cell|
+        if cell == @ai_choice
+          @cells[@ai_choice - 1] = :O
+          puts "Computer chooses #{@ai_choice}."
+          return true
+        end
+      end
+    end
   end
 
   def play_1p_O
@@ -137,16 +247,102 @@ class Game
     play_again
   end
 
-  def ai_X
-    @ai_choice = rand(1..9)
+  def puts_O_down
+    puts "#{@name2}'s turn."
+    print "Enter your choice:"
+    @p2_choice = gets.chomp.to_i
     @cells.map do |cell|
-      if cell == @ai_choice
-        @cells[@ai_choice - 1] = :X
-        puts "Computer chooses #{@ai_choice}."
+      if cell == @p2_choice
+        @cells[@p2_choice - 1] = :O
         return true
       end
     end
     return false
+  end
+
+  def play_1p_Adv_O
+    loop do
+      until ai_adv_X
+      end
+      @count += 1
+      board
+      break if @count ==9
+      until puts_O_down
+        puts "Try again!"
+      end
+      @count += 1
+      board
+      break if @count ==9
+    end
+    puts "It's a tie!"
+    play_again
+  end
+
+  def ai_adv_X
+    if check_win_X == true
+      return true
+    elsif check_loss_X == true
+      return true
+    else ai_X
+      return true
+    end
+  end
+
+  def check_win_X
+    @win2.each do |inner|
+      smart_ai = inner.reject do |value|
+        value == :C
+      end
+      smart_ai.select! do |value|
+        if smart_ai.length == 1 && value != :O
+          @ai_choice = value
+          @cells.map do |cell|
+            if cell == @ai_choice
+              @cells[@ai_choice - 1] = :X
+              puts "Computer chooses #{@ai_choice}."
+              return true
+            end
+          end
+        else
+          value
+        end
+      end
+    end
+  end
+
+  def check_loss_X
+    @win2.each do |inner|
+      smart_ai = inner.reject do |value|
+        value == :O
+      end
+      smart_ai.select! do |value|
+        if smart_ai.length == 1 && value != :C
+          @ai_choice = value
+          @cells.map do |cell|
+            if cell == @ai_choice
+              @cells[@ai_choice - 1] = :X
+              puts "Computer chooses #{@ai_choice}."
+              return true
+            end
+          end
+        else
+          value
+        end
+      end
+    end
+  end
+
+  def ai_X
+    loop do
+      @ai_choice = rand(1..9)
+      @cells.map do |cell|
+        if cell == @ai_choice
+          @cells[@ai_choice - 1] = :X
+          puts "Computer chooses #{@ai_choice}."
+          return true
+        end
+      end
+    end
   end
 
   def play_2p
@@ -169,32 +365,6 @@ class Game
     play_again
   end
 
-  def puts_X_down
-    puts "#{@p1.name}'s turn."
-    print "Enter your choice:"
-    @p1_choice = gets.chomp.to_i
-    @cells.map do |cell|
-      if cell == @p1_choice
-        @cells[@p1_choice - 1] = :X
-        return true
-      end
-    end
-    return false
-  end
-
-  def puts_O_down
-    puts "#{@p2.name}'s turn."
-    print "Enter your choice:"
-    @p2_choice = gets.chomp.to_i
-    @cells.map do |cell|
-      if cell == @p2_choice
-        @cells[@p2_choice - 1] = :O
-        return true
-      end
-    end
-    return false
-  end
-
   def tally
     @win2.each do |inner|
       inner.map! do |value|
@@ -214,10 +384,10 @@ class Game
   def winner
     @win2.each do |inner|
       if inner == [:X,:X,:X]
-        puts "#{@p1.name} wins!"
+        puts "#{@name1} wins!"
         play_again
       elsif inner == [:O,:O,:O]
-        puts "#{@p2.name} wins!"
+        puts "#{@name2} wins!"
         play_again
       elsif inner == [:C,:C,:C]
         puts "Computer wins!"
@@ -238,6 +408,10 @@ class Game
         play_1p_X
       elsif defined?(@p2.name) && !defined?(@p1.name)
         play_1p_O
+      elsif defined?(@p1adv.name)
+        play_1p_Adv_X
+      elsif defined?(@p2adv.name)
+        play_1p_Adv_O
       end
     elsif rematch == "N"
       puts "Would you like to start a new game? (Y)es or (N)o?"
@@ -245,6 +419,8 @@ class Game
       if new_game == "Y"
         @p1 = nil
         @p2 = nil
+        @p1adv = nil
+        @p2adv = nil
         initialize
         ai_or_2p
       else
